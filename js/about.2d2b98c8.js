1927fff2b3f3c5e32e71379aafd3916cda3042f5
qurben <qurben@users.noreply.github.com>
2021-08-02 14:02:16 +0000

    deploy: 930fca4448a157dd96f34f0e7fff646a304358d3

---
+++ js/about.2d2b98c8.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(n,t,a){"use strict";a.r(t);var e=a("7a23"),o={class:"about"},c=Object(e["o"])("h1",null,"This is an about page",-1);function s(n,t){return Object(e["F"])(),Object(e["k"])("div",o,[c])}const u={};u.render=s;t["default"]=u}}]);
+//# sourceMappingURL=about.2d2b98c8.js.map
