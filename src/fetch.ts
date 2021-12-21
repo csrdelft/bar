6b02be527fb321961cec649fd6191db86eb784fe
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2021-12-21 12:32:11 +0100

    Verwijder axios

--- src/fetch.ts
+++ src/fetch.ts
@@ -1,10 +1,10 @@
-import axios, {AxiosRequestConfig} from 'axios';
 import router from '@/router';
 import {getLocatieToken, getToken} from '@/token';
+import {RequestObject} from "client-oauth2";
 
-type FetchConfig = AxiosRequestConfig & { url: string }
+type RequestObj = RequestObject & { headers: Record<string, string> }
 
-export const fetchAuthorized = async <T>(requestObj: FetchConfig): Promise<T> => {
+export const fetchAuthorized = async <U, T extends RequestObj = any>(requestObj: T): Promise<U> => {
   const token = getToken();
 
   if (!token) {
@@ -18,7 +18,7 @@ export const fetchAuthorized = async <T>(requestObj: FetchConfig): Promise<T> =>
   const tokenHeader: Record<string, string> = locatieToken ? {'X-Bar-Token': locatieToken.sleutel} : {}
 
   try {
-    const response = await axios(token.sign({
+    const {url, ...rest} = token.sign({
       ...requestObj,
       headers: {
         ...requestObj.headers,
@@ -26,9 +26,11 @@ export const fetchAuthorized = async <T>(requestObj: FetchConfig): Promise<T> =>
         'Content-Type': 'application/json',
       },
       url: process.env.VUE_APP_REMOTE_URL + requestObj.url,
-    }));
+    });
 
-    return response.data;
+    const response = await fetch(url, rest);
+
+    return await response.json() as U;
   } catch (e) {
     if (e.response && e.response.status === 401) {
       await router.push('/auth/logout');
