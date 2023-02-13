07f8dd6bf86b9063732d5f099a0aa33fa9813de4
Sebastiaan Benjamins <45468731+cbasje@users.noreply.github.com>
2023-02-13 22:28:43 +0100

    Prettier

--- src/auth/csrAuth.ts
+++ src/auth/csrAuth.ts
@@ -1,11 +1,13 @@
-import ClientOAuth2 from 'client-oauth2';
+import ClientOAuth2 from "client-oauth2";
 
+// FIXME: redirectUri is 'http://localhost:3000/auth/callback'
 const csrAuth = new ClientOAuth2({
   clientId: process.env.VUE_APP_OAUTH2_CLIENT_ID,
   authorizationUri: `${process.env.VUE_APP_REMOTE_URL}/authorize`,
   accessTokenUri: `${process.env.VUE_APP_REMOTE_URL}/api/v3/token`,
   redirectUri: `${window.location.origin}/auth/callback`,
-  scopes: ['BAR:NORMAAL', 'BAR:BEHEER', 'BAR:TRUST'],
+  scopes: ["BAR:NORMAAL", "BAR:BEHEER", "BAR:TRUST"],
 });
 
 export default csrAuth;
+
