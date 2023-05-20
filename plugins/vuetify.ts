839f06e9b613306e66fb4824be251c28e3614379
Sebastiaan Benjamins <45468731+cbasje@users.noreply.github.com>
2023-05-20 16:17:14 +0200

    Update vuetify.ts

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,7 +1,25 @@
+import "@fortawesome/fontawesome-free/css/all.css";
+
+// Vuetify
+import "vuetify/styles";
 import { createVuetify } from "vuetify";
+import { aliases, fa } from "vuetify/iconsets/fa";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
 
 export default defineNuxtPlugin((nuxtApp) => {
-  const vuetify = createVuetify();
+  const vuetify = createVuetify({
+    ssr: true,
+    components,
+    directives,
+    icons: {
+      defaultSet: "fa",
+      aliases,
+      sets: {
+        fa,
+      },
+    },
+  });
 
   nuxtApp.vueApp.use(vuetify);
 });
