dda5e7e9bb63bf9d7a38c8bd6a15ff56328647fb
Sebastiaan Benjamins <45468731+cbasje@users.noreply.github.com>
2023-05-28 22:45:54 +0200

    Fix theme

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,7 +1,19 @@
-import { createVuetify } from "vuetify";
-import { aliases, fa } from "vuetify/iconsets/fa";
+import { ThemeDefinition, createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
+import { aliases, fa } from "vuetify/iconsets/fa";
+import { nl } from "vuetify/locale";
+
+const myDarkTheme: ThemeDefinition = {
+  dark: true,
+  colors: {
+    primary: "#607D8B",
+    error: "#B00020",
+    info: "#2196F3",
+    success: "#4CAF50",
+    warning: "#FB8C00",
+  },
+};
 
 export default defineNuxtPlugin((nuxtApp) => {
   const vuetify = createVuetify({
@@ -14,6 +26,16 @@ export default defineNuxtPlugin((nuxtApp) => {
       sets: {
         fa,
       },
+    },
+    theme: {
+      defaultTheme: "myDarkTheme",
+      themes: {
+        myDarkTheme,
+      },
+    },
+    locale: {
+      locale: "nl",
+      messages: { nl },
     },
   });
 
