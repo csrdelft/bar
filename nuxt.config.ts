039add77861c721dbdee654e1b1bab8e78be272e
Sebastiaan Benjamins <45468731+cbasje@users.noreply.github.com>
2023-06-01 16:09:36 +0200

    Add nuxt-session

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,7 +15,8 @@ export default defineNuxtConfig({
   },
 
   buildModules: ["nuxt-typed-router"],
-  modules: ["@pinia/nuxt", "@vueuse/nuxt"],
+  modules: ["@sidebase/nuxt-session", "@pinia/nuxt", "@vueuse/nuxt"],
+
   css: ["@fortawesome/fontawesome-free/css/all.min.css", "vuetify/lib/styles/main.sass"],
   build: {
     transpile: ["vuetify", "@vuepic/vue-datepicker"],
@@ -23,6 +24,12 @@ export default defineNuxtConfig({
   vite: {
     define: {
       "process.env.DEBUG": false,
+    },
+  },
+
+  session: {
+    session: {
+      cookieSameSite: "strict",
     },
   },
 });
