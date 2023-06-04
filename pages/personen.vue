311cc35bb4e3a07e2194f5c92dbff18aa9b502ca
Sebastiaan Benjamins <45468731+cbasje@users.noreply.github.com>
2023-06-04 18:36:34 +0200

    Move LedenTable logic

--- pages/personen.vue
+++ pages/personen.vue
@@ -1,25 +1,61 @@
 <script lang="ts" setup>
 import Keyboard from "~/components/Keyboard.vue";
-import LedenTable from "~/components/LedenTable.vue";
+import { VDataTable } from "vuetify/lib/labs/components.mjs";
+import { Persoon } from "~/types/persoon";
+import { useTypedRouter } from "~/generated";
+
+const { bedragFormat } = useFormatter();
+const { router, routes } = useTypedRouter();
 
 const query = ref("");
-const upperValue = computed(() => query.value.toUpperCase());
-const change = (value: string) => {
-  query.value = value;
+
+const {
+  data: personen,
+  error,
+  pending,
+} = useAsyncData("personen", async () => {
+  return await fetchAuthorized<Persoon[]>("/api/v3/bar/personen");
+});
+
+const headers = [
+  { title: "Bijnaam", key: "naam" },
+  { title: "Naam", key: "weergave" },
+  { title: "Saldo", key: "saldo" },
+];
+
+const rowClick = (row: any) => {
+  router.push({ name: routes.invoerSlug, params: { slug: row.uid } });
+};
+
+const getColor = (bedrag: number) => {
+  if (bedrag > 10) return "success";
+  else if (bedrag >= 0) return "warning";
+  else return "error";
 };
 
 definePageMeta({
   middleware: ["token"],
 });
-
-const henk = async () => {
-  const data = await fetchAuthorized("/api/v3/personen");
-};
 </script>
 
 <template>
   <Keyboard v-model="query" placeholder="Zoeken" :grab-focus="true" />
-  <LedenTable :zoeken="query" />
+  <!-- <LedenTable :items="filteredPersonen" :pending="pending" /> -->
+  <v-data-table
+    :headers="headers"
+    :items="personen ?? undefined"
+    @click:row="rowClick"
+    :items-per-page="50"
+    :loading="pending"
+    :sortBy="[{ key: 'recent', order: 'desc' }]"
+    :search="query"
+  >
+    <template v-slot:item.saldo="{ item }">
+      <v-chip :color="getColor(item.columns.saldo)">
+        {{ bedragFormat(item.columns.saldo) }}
+      </v-chip>
+    </template>
+  </v-data-table>
 </template>
 
 <style>
